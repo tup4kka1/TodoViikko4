510a4e92b28130cad03570ac973c720612f579f7
tupa4kka1 <niilo.tanhuala@gmail.com>
2026-02-02 16:23:53 +0200

    list

--- App.tsx
+++ App.tsx
@@ -1,10 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Pressable,  } from 'react-native';
+import { SwipeListView } from 'react-native-swipe-list-view';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+interface Item {
+  id: string;
+  name: string;
+  done?: boolean;
+}
 
 export default function App() {
+  const [items, setItems] = useState<Item[]>([]);
+  const [input, setInput] = useState("")
+  const [donem, setDone] = useState(false);
+
+
+  const addItem = () => {
+    if (input.trim()) {
+      setItems(prev => [
+        ...prev,
+        { id: Date.now().toString(), name: input.trim(), done: false},
+      ]);
+      setInput("")
+      AsyncStorage.setItem("", input);
+    }
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{padding: 20}}>TODO list</Text>
+      <View style={{flexDirection: "row"}}> <TextInput value={input} onChangeText={setInput} placeholder='Enter task'/> <Button title="Save" onPress={addItem} /> </View>
+      
+      <SwipeListView style={{padding:20}} data={items} keyExtractor={item => item.id} renderItem={({item}) => (
+        <View>
+          <Pressable onPress={() => {
+            item.done = !item.done
+            setDone(!donem)
+          }}>
+            <Text style={{textDecorationLine: item.done ? "line-through" : "none"}}>{item.name}</Text>
+          </Pressable>
+        </View>
+        
+      )}
+      />
       <StatusBar style="auto" />
     </View>
   );
